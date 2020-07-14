940feccf0fbf56df6945474591c82295a11b8697
Raun G <rgreen@creativeanvil.com>
2020-07-14 12:30:52 -0500

    first commit

---
+++ assets/js/realEstate/realEstate.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Header from './Header';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: 'Joe',
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+const app = document.getElementById('app');
+
+ReactDOM.render(<App />, app);
